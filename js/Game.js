4b6ad8014d539230a199602704eff33709357885
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-08-04 23:34:13 -0400

    Add class constructors, create phrase, and get random phrase methods

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+
+    /**
+     * Creates phrases for use in the game
+     * @return {Object}  -  An array of phrases
+     */
+    createPhrases() {
+        const phrases = [new Phrase('Twin Peaks'),
+                         new Phrase('The Twilight Zone'),
+                         new Phrase('The Kids in the Hall'),
+                         new Phrase('Star Trek'),
+                         new Phrase('Mystery Science Theater')];
+        return phrases;
+    }
+
+    /**
+     * Retrieves a phrase at random from the array of phrases
+     * @return {array[x]}  -  An element from an array
+     */
+    getRandomPhrase() {
+        this.activePhrase = this.phrases[ Math.floor(Math.random() * this.phrases.length )];
+        return this.activePhrase;
+    }
+
+
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Displays the phrase on the screen
+     * @param {*} phrase 
+     */
+    addPhraseToDisplay() {
+        
+    }
+
+
+
+ }
